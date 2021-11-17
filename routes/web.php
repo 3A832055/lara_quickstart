ced1bb4d916b0547342dd93359d7ab7ab2a3a930
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-11-17 19:49:04 +0800

    初步建置三個路由(Routes)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Task;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //
 });
+
+   Route::post('/task', function (Request $request) {
+    //
+   });
+
+   Route::delete('/task/{task}', function (Task $task) {
+    //
+   });
+   
